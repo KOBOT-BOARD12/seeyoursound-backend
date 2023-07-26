027efeb48c9b4d4f862d19f5fe6018b39699f588
changyeop1 <changyeop@comcom.ai>
2023-07-26 15:21:32 +0900

    feat: split router

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
-from typing import Union
-
 from fastapi import FastAPI
+from router.streaming import ws_router
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(ws_router)
