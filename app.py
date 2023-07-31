a6e1ab1c30992604199a4274191783384858a872
syngrxm <smcubez48@gmail.com>
2023-07-31 13:10:46 +0900

    feat: delete router upload

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from router.streaming import ws_router
+from router.delete_keywords import delete_keywords_router
 
 app = FastAPI()
 
 app.include_router(ws_router)
+app.include_router(delete_keywords_router)
