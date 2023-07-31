087a6b42fadb3e047e4e402b44affcc4458e1ea9
syngrxm <smcubez48@gmail.com>
2023-07-31 18:44:47 +0900

    put the keywords file together

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from manager import firebase_manager
 from router.streaming import ws_router
-from router.delete_keywords import delete_keywords_router
-from router.check_keywords import existing_keyword_router
-from router.register_keywords import register_keyword_router
+from router.keywords_router.delete_keywords import delete_keywords_router
+from router.keywords_router.check_keywords import existing_keyword_router
+from router.keywords_router.register_keywords import register_keyword_router
 from router.model_router.upload_model import upload_model_router
 
 app = FastAPI()
