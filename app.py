9128d75b1bb3428bd65e2af3f8d7873e14e0460f
syngrxm <smcubez48@gmail.com>
2023-07-31 11:22:10 +0900

    fix: cause errors

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from router.register_keywords import ws_router
+from router.register_keywords import router
 from manager import firebase_manager
 from router.streaming import ws_router
 
 app = FastAPI()
 
 app.include_router(ws_router)
+app.include_router(router)
--- router/register_keywords.py
+++ router/register_keywords.py
@@ -1,11 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from manager.firebase_manager import firestore, db
 from firebase_admin.firestore import ArrayUnion
 from pydantic import BaseModel
-from typing import List
 import re
 
-ws_router = APIRouter()
+router = APIRouter()
 
 class UserData(BaseModel):
     user_inherent_id: str
@@ -13,15 +12,14 @@ class UserData(BaseModel):
 
 def validate_user_data(user_data: UserData):
     if not user_data.user_new_keyword:
-        raise ValueError("키워드를 입력해 주세요. :)")
+        raise HTTPException(status_code=400, detail="키워드를 입력해 주세요.")
     if len(user_data.user_new_keyword) > 5:
-        raise ValueError("키워드는 5글자 이하로 입력해 주세요. :)")
+        raise HTTPException(status_code=400, detail="키워드는 다섯 글자 이하로 입력해 주셔야 합니다.")
     if not re.match(r'^[가-힣]+$', user_data.user_new_keyword):
-        raise ValueError("키워드는 한글로만 입력해 주세요. :)")
+        raise HTTPException(status_code=400, detail="키워드는 한글로만 입력이 가능합니다.")
 
-@ws_router.post("/receive_data")
+@router.post("/receive_data")
 async def receive_data(user_data: UserData):
-    try:
         validate_user_data(user_data)
         keywords_ref = db.collection("Users").document(user_data.user_inherent_id)
         doc = keywords_ref.get()
@@ -29,10 +27,10 @@ async def receive_data(user_data: UserData):
             existing_keywords = doc.to_dict().get("keywords", []) # 사용자의 고유 아이디 컬렉션에서 이미 존재하고 있는 키워드 가져오기
 
             if user_data.user_new_keyword in existing_keywords:
-                raise ValueError("이미 존재하는 키워드입니다.")
+                raise HTTPException(status_code=400, detail="이미 존재하는 키워드입니다.")
 
             if len(existing_keywords) >= 5:
-                raise ValueError("키워드는 최대 5개까지만 추가할 수 있습니다.")
+                raise HTTPException(status_code=400, detail="키워드는 최대 다섯 개까지만 추가할 수 있습니다.")
 
             keywords_ref.update({
                 "keywords": firestore.ArrayUnion([user_data.user_new_keyword])
@@ -41,5 +39,4 @@ async def receive_data(user_data: UserData):
             keywords_ref.set({
                 "keywords": [user_data.user_new_keyword]
             })
-    except ValueError as e:
-        print(f"오류 발생: {e}")
+        return {"message": "키워드가 성공적으로 추가되었습니다."}
