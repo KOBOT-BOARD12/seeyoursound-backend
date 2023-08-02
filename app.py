2bcc51c76a9a95d1b9ff72767056ec76945a4179
syngrxm <smcubez48@gmail.com>
2023-08-02 18:41:36 +0900

    fix: router path

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 from fastapi import FastAPI
 from manager import firebase_manager
 from router.streaming import ws_router
-from router.keywords_router.delete_keywords import delete_keywords_router
-from router.keywords_router.check_keywords import existing_keyword_router
-from router.keywords_router.register_keywords import register_keyword_router
-from router.model_router.upload_model import upload_model_router
+from router.keyword_router import register_keyword_router, existing_keyword_router, delete_keywords_router
+from router.model_router import upload_model_router
 
 app = FastAPI()
 
 app.include_router(ws_router)
 app.include_router(register_keyword_router)
-app.include_router(upload_model_router)
-app.include_router(delete_keywords_router)
 app.include_router(existing_keyword_router)
-app.include_router(register_keyword_router)
+app.include_router(delete_keywords_router)
+app.include_router(upload_model_router)
