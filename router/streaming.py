3ed7f05dbcfb0a68d7f0f93c2af05cda3e1fe763
changyeop1 <changyeop@comcom.ai>
2023-08-11 15:50:48 +0900

    feat: split audio data by chunk count

--- router/streaming.py
+++ router/streaming.py
@@ -1,31 +1,49 @@
+import wave
+import json
+import base64
+import configparser as parser
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+
+properties = parser.ConfigParser()
+properties.read("config.ini")
 
 ws_router = APIRouter()
 
-@ws_router.websocket("/audio_ws")
-async def audio_websocket_endpoint(websocket: WebSocket):
+def bytes_to_wav(bytes_data, file_name):
+    wav_detail = properties["WAV_DETAIL"]
+    with wave.open("data/" + file_name, 'wb') as wf:
+        wf.setnchannels(wav_detail["num_channels"])
+        wf.setsampwidth(wav_detail["sample_width"])
+        wf.setframerate(wav_detail["sample_rate"])
+        wf.writeframes(bytes_data)
+    wf.close()
+
+@ws_router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    chunk_count = properties["AUDIO_DATA"]["chunk_count"]  
+    top_channel_arr = []
+    bottom_channel_arr = []
     await websocket.accept()
     try:
-        print("Audio WebSocket connected.")
+        print("WebSocket connected.")
         while True:
             data = await websocket.receive_text()
-            # TODO: data processing
-            pass
+            json_data = json.loads(data)
+
+            top_channel, bottom_channel = json_data["top_channel"], json_data["bottom_channel"]
+            top_channel_data, bottom_channel_data = bytes(base64.b64decode(top_channel.encode('utf-8'))), bytes(base64.b64decode(bottom_channel.encode('utf-8')))
+            if len(top_channel_data) == 0 or len(bottom_channel_data) == 0:
+                continue
+            top_channel_arr.append(top_channel_data)
+            bottom_channel_arr.append(bottom_channel_data)
+            if len(top_channel_arr) == chunk_count and len(bottom_channel_arr) == chunk_count:
+                bytes_to_wav(bytes(bytearray(b''.join(top_channel_arr))), "top_channel.wav")
+                bytes_to_wav(bytes(bytearray(b''.join(bottom_channel_arr))), "bottom_channel.wav")
+                top_channel_arr.pop(0)
+                bottom_channel_arr.pop(0)
+
+            gy_x, gy_y, gy_z = json_data["gy_x"], json_data["gy_y"], json_data["gy_z"]
+            # TODO : send data to model serving server
 
     except WebSocketDisconnect:
-        print("Audio WebSocket disconnected.")
-        pass
-
-@ws_router.websocket("/gyro_ws")
-async def gyro_websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    try:
-        print("Gyro WebSocket connected.")
-        while True:
-            data = await websocket.receive_text()
-            # TODO: data processing
-            pass
-
-    except WebSocketDisconnect:
-        print("Gyro WebSocket disconnected.")
-        pass
+        print("WebSocket closed.")
