9fdebbf3342e76f382386d41a56006317e0a2214
changyeop1 <changyeop@comcom.ai>
2023-08-25 12:47:11 +0900

    feat: get model prediction from model server

--- router/websocket.py
+++ router/websocket.py
@@ -1,8 +1,13 @@
-import wave
 import json
-import base64
 import configparser as parser
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect, HTTPException, UploadFile, File, Body
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect, HTTPException
+import requests
+from os.path import os, join, dirname
+
+from dotenv import load_dotenv, find_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(find_dotenv())
 
 properties = parser.ConfigParser()
 properties.read("config.ini")
@@ -11,14 +16,6 @@ ws_router = APIRouter()
 model_predictions_router = APIRouter()
 
 connected_websocket = None
-def bytes_to_wav(bytes_data, file_name):
-    wav_detail = properties["WAV_DETAIL"]
-    with wave.open("data/" + file_name, 'wb') as wf:
-        wf.setnchannels(int(wav_detail["num_channels"]))
-        wf.setsampwidth(int(wav_detail["sample_width"]))
-        wf.setframerate(int(wav_detail["sample_rate"]))
-        wf.writeframes(bytes_data)
-    wf.close()
 
 @ws_router.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -34,29 +31,25 @@ async def websocket_endpoint(websocket: WebSocket):
             data = await websocket.receive_text()
             json_data = json.loads(data)
 
-            top_channel, bottom_channel = json_data["top_channel"], json_data["bottom_channel"]
-            top_channel_data, bottom_channel_data = bytes(base64.b64decode(top_channel.encode('utf-8'))), bytes(base64.b64decode(bottom_channel.encode('utf-8')))
-            if len(top_channel_data) == 0 or len(bottom_channel_data) == 0:
+            top_channel, bottom_channel, uid = json_data["top_channel"], json_data["bottom_channel"], json_data["uid"]
+            if len(top_channel) == 0 or len(bottom_channel) == 0:
                 continue
-            top_channel_arr.append(top_channel_data)
-            bottom_channel_arr.append(bottom_channel_data)
+            top_channel_arr.append(top_channel)
+            bottom_channel_arr.append(bottom_channel)
+
             if len(top_channel_arr) == chunk_count and len(bottom_channel_arr) == chunk_count:
-                bytes_to_wav(bytes(bytearray(b''.join(top_channel_arr))), "top_channel.wav")
-                bytes_to_wav(bytes(bytearray(b''.join(bottom_channel_arr))), "bottom_channel.wav")
+                data = {"top_channel" : ''.join(top_channel_arr), "bottom_channel_arr" : ''.join(bottom_channel_arr), "uid" : uid}
+                requests.post(os.getenv("MODEL_SERVER_URL") + "/prediction", data=json.dumps(data), headers={"Content-Type": "application/json"})
                 top_channel_arr.pop(0)
                 bottom_channel_arr.pop(0)
-
-            gy_x, gy_y, gy_z = json_data["gy_x"], json_data["gy_y"], json_data["gy_z"]
-            # TODO : send data to model serving server
 
     except WebSocketDisconnect:
         print("WebSocket closed.")
         connected_websocket = None
 
-@model_predictions_router.post("/model_predictions")
-async def model_predictions(model_result: dict = Body(None)):
-    prediction = await model_result.read()
+@model_predictions_router.post("/get_model_prediction")
+async def get_model_prediction(model_result: dict):
     if connected_websocket:
-        await connected_websocket.send_text(prediction) 
+        await connected_websocket.send_text(json.dumps(model_result)) 
     else:
         raise HTTPException(status_code=400, detail="연결된 웹소켓이 존재하지 않습니다.")
