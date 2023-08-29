d6442d2ee81c983c9bb3c412c8f66ec4b1af78d8
syngrxm <smcubez48@gmail.com>
2023-08-29 16:39:23 +0900

    feat: edit app.py to add new router

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from manager import firebase_manager
 from router.websocket import ws_router
 from router.keyword_router import keyword_router
 from router.model_router import upload_model_router
+from router.class_router import class_router
 
 app = FastAPI()
 
 app.include_router(ws_router)
 app.include_router(keyword_router)
 app.include_router(upload_model_router)
+app.include_router(class_router)
