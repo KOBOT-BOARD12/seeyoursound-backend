9626a42afaa545bfa65cfff2d6e7314ebc02bedc
syngrxm <smcubez48@gmail.com>
2023-08-30 17:04:47 +0900

    fix: post form

--- router/class_router.py
+++ router/class_router.py
@@ -5,26 +5,27 @@ from typing import Dict
 
 class_router = APIRouter()
 
-class SoundClass(BaseModel):
-    class_config: Dict[int, bool]
-
-class ClassSetting(BaseModel):
+class ClassData(BaseModel):
     user_id: str
-    sound_class: SoundClass
+    sound_class: Dict[int, bool]
 
 @class_router.post("/select_class")
-async def select_class(data: ClassSetting):
+async def select_class(data: ClassData):
     try:
         user_id = data.user_id
-        class_config = data.sound_class.class_config
-        class_config_str_keys = {str(key): value for key, value in class_config.items()}
+        current_class = data.sound_class
+
+        new_class_dictionary = {str(key): value for key, value in current_class.items()}
+
         user_doc_ref = db.collection("Users").document(user_id)
         user_doc = user_doc_ref.get()
+
         if user_doc.exists:
-            existing_class_config = user_doc.to_dict().get("class_config", {})
-            existing_class_config.update(class_config_str_keys)
-            user_doc_ref.update({"class_config": existing_class_config})
+            existing_class = user_doc.to_dict().get("current_class", {})
+            existing_class.update(new_class_dictionary)
+            user_doc_ref.update({"current_class": existing_class})
         else:
-            user_doc_ref.set({"class_config": class_config_str_keys})
+            user_doc_ref.set({"current_class": new_class_dictionary}) 
+
     except:
-        HTTPException(status_code=400, detail="잘못된 요청이 들어왔습니다.")
+        raise HTTPException(status_code=400, detail="잘못된 요청이 들어왔습니다.")
