d9203ab94e3c5fd010472afd742f65da2871465c
changyeop1 <changyeop@comcom.ai>
2023-09-04 14:58:43 +0900

    feat: apply code formatter

--- router/class_router.py
+++ router/class_router.py
@@ -5,12 +5,15 @@ from typing import Dict
 
 class_router = APIRouter()
 
+
 class ClassData(BaseModel):
     user_id: str
     sound_class: Dict[int, bool]
 
+
 class ReturnData(BaseModel):
     user_id: str
+
 
 @class_router.post("/update_class")
 async def update_class(data: ClassData):
@@ -26,16 +29,17 @@ async def update_class(data: ClassData):
             existing_class.update(new_class_dictionary)
             user_doc_ref.update({"current_class": existing_class})
         else:
-            user_doc_ref.set({"current_class": new_class_dictionary}) 
+            user_doc_ref.set({"current_class": new_class_dictionary})
     except:
         raise HTTPException(status_code=400, detail="잘못된 요청이 들어왔습니다.")
+
 
 @class_router.post("/return_class")
 async def return_class(data: ReturnData):
     user_id = data.user_id
     user_doc_ref = db.collection("Users").document(user_id)
     user_info = user_doc_ref.get()
-    
+
     if user_info.exists and user_info.to_dict().get("current_class"):
         existing_current_class = user_info.to_dict()["current_class"]
         return existing_current_class
@@ -43,5 +47,5 @@ async def return_class(data: ReturnData):
         initial_value = {str(key): True for key in range(5)}
         existing_data = user_info.to_dict() if user_info.exists else {}
         existing_data["current_class"] = initial_value
-        user_doc_ref.set(existing_data) 
+        user_doc_ref.set(existing_data)
         return initial_value
